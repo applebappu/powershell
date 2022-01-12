bcac6fe269ccbbec6f8845357ad848784ed4689e
applebappu <87842348+applebappu@users.noreply.github.com>
2022-01-12 11:43:52 -0900

    Add files via upload

---
+++ t-x.ps1
@@ -0,0 +1,45 @@
+﻿# T-X
+# by Jo Anne Wilson, 2022.01.12
+# Terminates a list of users from a .csv
+
+$targetlist = import-csv "C:\Users\jo\dev\scripts\Terminator\Nologin_01.12.22.csv"
+$targets = $targetlist.samaccountname
+$date = get-date -f "MMddyyyy"
+$descdate = get-date -f "MM-dd-yyyy"
+$sevenzip = "C:\Program Files\7-Zip\7z.exe"
+$wordlist = import-csv "C:\Users\jo\dev\scripts\Terminator\WordList.csv"
+$list1 = $wordlist.List1
+$list2 = $wordlist.List2
+$group = get-adgroup "Domain Users" -properties @("primaryGroupToken")
+
+foreach ($target in $targets) {
+    $homedir = "\\Sitka\Users\$target"
+    $althomedir = "\\Sitfs01\Users\$target"
+    $archive = "\\Sitnas5\HomeArchives\Sitka$target$date.7z"
+    $altarchive = "\\Sitnas5\HomeArchives\Sitfs01$target$date.7z"
+
+    if (test-path $homedir){
+	    (start-process -filepath $sevenzip -argumentlist "a -t7z $archive $homedir" -wait -passthru)
+        remove-item $homedir -recurse -force
+    }
+    elseif (test-path $althomedir){
+	    (start-process -FilePath $sevenzip -argumentlist "a -t7z $altarchive $althomedir" -wait -passthru)
+        remove-item $althomedir -recurse -force
+    }
+    $word1 = get-random -inputobject $list1 -count 1
+    $word2 = get-random -inputobject $list2 -count 1
+    $num = get-random -minimum 10 -maximum 99
+    $newpass = $word1+$word2+$num
+    set-adaccountpassword $target -reset -newpassword (convertto-securestring -asplaintext $newpass -force) 
+    get-aduser -identity $target | set-adobject -replace @{msexchhidefromaddresslists=$true}
+    get-aduser -identity $target -properties mailnickname | set-aduser -replace @{mailnickname=$target}
+    get-aduser -identity $target | set-aduser -replace @{primarygroupid=$group.primarygrouptoken}
+    get-aduser -identity $target -properties memberof | foreach-object {
+        $_.memberof | remove-adgroupmember -members $_.distinguishedname -confirm:$false
+    }
+    clear-adaccountexpiration $target
+    set-aduser $target -clear telephonenumber, pager, ipphone, facsimileTelephoneNumber, mail
+    set-aduser $target -description $descdate
+    disable-adaccount $target
+    get-aduser $target | move-adobject -targetpath "OU=Disabled Accounts,OU=SEARHC Users,DC=intranet,DC=searhc,DC=org"
+}
---
+++ unnamed O365 terminator.ps1
@@ -0,0 +1,104 @@
+﻿# T-X
+# by Jo Anne Wilson, 2021.05.25
+# strips O365 licenses from a list of users
+
+$targetlist = import-csv "C:\Users\jo\dev\scripts\Terminator\expiredusersterminated-05252021.csv"
+$targets = $targetlist.samaccountname
+
+connect-azuread
+connect-msolservice
+
+function Sync-ADDomain {
+<# 
+        .SYNOPSIS 
+            Emulates the repadmin /syncall to force AD replication 
+     
+        .DESCRIPTION 
+            Author:          Collin Chaffin 
+            Description:    This function emulates the repadmin /syncall to 
+                            force AD replication across all sites and domain 
+                            controllers.  At the time I wrote this I could not 
+                            find any example or suitable replacement to calling 
+                            the repadmin binary 
+     
+        .EXAMPLE 
+            C:\> Sync-ADDomain 
+            Forcing Replication on WIN2008R2-DC1.lab.local 
+            Forcing Replication on WIN2008R2-DC2.lab.local 
+            Forcing Replication on WIN2008R2-DC3.lab.local 
+     
+        .EXAMPLE 
+            C:\> Sync-ADDomain -WhatIf 
+            What if: Performing operation "Forcing Replication" on Target "WIN2008R2-DC1.lab.local". 
+            What if: Performing operation "Forcing Replication" on Target "WIN2008R2-DC2.lab.local". 
+            What if: Performing operation "Forcing Replication" on Target "WIN2008R2-DC3.lab.local". 
+    #>
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  param(
+  )
+  begin
+  {
+    Write-Debug "Sync-ADDomain function started."
+
+    try
+    {
+      # Set up the AD object and retrieve operator's current AD domain 
+      $adDomain = $env:userdnsdomain
+      Write-Debug "Detected operators AD domain as $($adDomain)"
+      $objADContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext ("Domain",$adDomain)
+      $domainControllers = [System.DirectoryServices.ActiveDirectory.DomainController]::FindAll($objADContext)
+    }
+    catch
+    {
+      #Throw terminating error 
+      throw $("ERROR OCCURRED DETERMINING USERDNSDOMAIN AND RETRIEVING LIST OF DOMAIN CONTROLLERS " + $_.Exception.Message)
+    }
+  }
+  process
+  {
+
+    try
+    {
+      # Cycle through all domain controllers emulating a repadmin /syncall 
+      foreach ($domainController in $domainControllers)
+      {
+        if ($PSCmdlet.ShouldProcess($domainController,"Forcing Replication"))
+        {
+          Write-Host "Forcing Replication on $domainController" -ForegroundColor Cyan
+          $domainController.SyncReplicaFromAllServers(([adsi]"").distinguishedName,'CrossSite')
+        }
+      }
+    }
+    catch
+    {
+      #Throw terminating error 
+      throw $("ERROR OCCURRED FORCING DIRECTORY SYNCHRONIZATION " + $_.Exception.Message)
+    }
+
+  }
+  end
+  {
+    Write-Debug "Sync-ADDomain function completed successfully."
+  }
+}
+
+Sync-ADDomain
+
+foreach ($target in $targets) {
+    $userlist = Get-AzureADUser -ObjectID $target+'@searhc.org'
+    $Skus = $userList | Select -ExpandProperty AssignedLicenses | Select SkuID
+    if($userList.Count -ne 0) {
+        if($Skus -is [array])
+        {
+            $licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+            for ($i=0; $i -lt $Skus.Count; $i++) {
+                $Licenses.RemoveLicenses +=  (Get-AzureADSubscribedSku | Where-Object -Property SkuID -Value $Skus[$i].SkuId -EQ).SkuID   
+            }
+            Set-AzureADUserLicense -ObjectId $Userlist -AssignedLicenses $licenses
+        } else {
+            $licenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+            $licenses.RemoveLicenses =  (Get-AzureADSubscribedSku | Where-Object -Property SkuID -Value $Skus.SkuId -EQ).SkuID
+            Set-AzureADUserLicense -ObjectId $Userlist -AssignedLicenses $licenses
+        }
+    }
+}
